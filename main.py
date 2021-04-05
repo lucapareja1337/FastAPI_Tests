5c668c2191848f2292c4b2cb1ed3944bf009d298
lucapareja1337 <lucapareja1337@gmail.com>
2021-04-05 12:44:26 -0300

    Protocolos http

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 from fastapi import FastAPI
+from typing import Optional
 
+from pydantic import BaseModel
 
+import uvicorn 
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':'Luca'}}
+@app.get('/blog')
+def index(limit=10, published : bool = True,sort: Optional[str] = None):
+   
+    if published:
+        return {'data':f'{limit} published blogs from the db list'}
+    else:
+        return {'data':f'{limit} blogs from the db list'}
 
-@app.get('/about')
-def about():
-    return {'data':{'about page'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    return{'data':{'1','2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):#Basicamente o uso do "blog" equivale a um request
+
+    return {'data': f"Blog is created with a title as {blog.title}"}
+
+
+#if __name__ =="__main__":
+ #   uvicorn.run(app,host="127.0.0.1",port=9000) #python main.py, feito para debug
