37db906fdfa3eaf89ca1794c1b07906e15ab5a18
Lucca <lucca@pop-os.localdomain>
2021-05-13 16:42:08 -0300

    Criaçao de users e tags

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,16 @@
+
+from typing import List
 from fastapi import FastAPI,Depends,status,Response,HTTPException
 from . import schemas,models
+from .hashing import Hash
 from fastapi.encoders import jsonable_encoder
 from .database import engine,SessionLocal
 from sqlalchemy.orm import Session
+
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
 
 def get_db():
     db = SessionLocal()
@@ -14,7 +19,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog',status_code=status.HTTP_201_CREATED)
+@app.post('/blog',status_code=status.HTTP_201_CREATED,tags=['blogs'])
 def create(request: schemas.Blog,db : Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
@@ -22,19 +27,19 @@ def create(request: schemas.Blog,db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=['blogs'])
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == id).update(jsonable_encoder(request))
     db.commit()
     return 'updated'
     
 
-@app.get('/blog')
+@app.get('/blog',response_model=List[schemas.ShowBlog],tags=['blogs'])
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
 def destroy(id,db : Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == 
                                         id).delete(synchronize_session=False)
@@ -42,7 +47,7 @@ def destroy(id,db : Session = Depends(get_db)):
     return 'done'
     
 
-@app.get('/blog/{id}',status_code=200)
+@app.get('/blog/{id}',status_code=200,response_model = schemas.ShowBlog,tags=['blogs'])
 def show(id,response: Response,db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -51,4 +56,23 @@ def show(id,response: Response,db : Session = Depends(get_db)):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return{'detal':f"Blog with the id {id} is not available"}
     return blog
+
+
+
+
+@app.post('/user',response_model = schemas.ShowUser,tags =['users'])
+def create_user(request:schemas.User,db: Session = Depends(get_db)):
     
+    new_user = models.User(name = request.name,email = request.email,password =Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/user',tags =['users'])
+def get_user(id:int,db:Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with the id {id} is not available")
+    return user
